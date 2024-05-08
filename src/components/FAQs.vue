71f7f241480126ecc5c1e00d3396d7191f80beb8
JoshEbay <162937690+JoshAEbay@users.noreply.github.com>
2024-05-08 13:38:05 +0800

    Editedit

--- src/components/FAQs.vue
+++ src/components/FAQs.vue
@@ -4,15 +4,14 @@ import CoordsFAQ from '../components/CoordsFAQ.vue';
 import HumidityFAQ from '../components/HumidityFAQ.vue';
 import WindFAQ from './WindFAQ.vue';
 import PressureFAQ from './PressureFAQ.vue';
+
+
 </script>
 
 <template>
   <div id="app">
     <router-view></router-view>
-    <div class="page">
-
-      <!-- <video :src="videoSource" autoplay loop muted playsinline class="video-background"></video> -->
-      
+    <div class="page">      
       <main class="main">
         <div class="container">
           <div class="laptop">
--- src/components/WindFAQ.vue
+++ src/components/WindFAQ.vue
@@ -1,18 +1,31 @@
+<script setup>
+import { inject } from 'vue'
+const globalSettings = inject('globalColor');
+
+
+</script>
+
 <template>
     <section class="section-bottom right">
-        <div class="block-bottom">
-            <div class="block-bottom-inner">
-                <div class="block-bottom-pic pic-wind">
-                   
-                </div>
-                <div class="block-bottom-texts">
-                    <div class="block-bottom-text-block">
-                        <div class="block-bottom-text-block-title">
-                            What is Wind?
-                        </div>
-                        <div class="block-bottom-text-block-desc">
-                            Wind is the horizontal movement of air in the atmosphere, caused by differences in air pressure. 
-                            It plays a significant role in weather patterns, affecting temperature, humidity, and precipitation distribution.
+        <div :class="{ 'light-mode': globalSettings === 'Light', 'dark-mode': globalSettings === 'Dark' }">
+            <div class="block-bottom">
+                <div class="block-bottom-inner">
+                    <div class="block-bottom-pic pic-wind">
+                    
+                    </div>
+                    <div class="block-bottom-texts">
+                        <div class="block-bottom-text-block">
+                            <div :class="{ 'light-mode': globalSettings === 'Light', 'dark-mode': globalSettings === 'Dark' }">
+                                <div class="block-bottom-text-block-title">
+                                    What is Wind?
+                                </div>
+                                <div :class="{ 'light-mode': globalSettings === 'Light', 'dark-mode': globalSettings === 'Dark' }">
+                                    <div class="block-bottom-text-block-desc">
+                                        Wind is the horizontal movement of air in the atmosphere, caused by differences in air pressure. 
+                                        It plays a significant role in weather patterns, affecting temperature, humidity, and precipitation distribution.
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
